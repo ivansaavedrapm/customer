cea0f85069231df1145953ce7584f6804527b9b0
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2025-09-05 11:04:40 -0600

    feature service region contract

--- src/main/java/com/customer/api/controller/CtrlRegion.java
+++ src/main/java/com/customer/api/controller/CtrlRegion.java
@@ -1,9 +1,9 @@
 package com.customer.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +21,7 @@ public class CtrlRegion {
 	SvcRegion svc;
 
 	@GetMapping
-	public List<Region> getRegions(){
+	public ResponseEntity<List<Region>> getRegions(){
 		return svc.getRegions();
 	}
 	
