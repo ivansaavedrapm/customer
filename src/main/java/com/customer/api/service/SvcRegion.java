b2b6df53cfd390e144374fcf671a782c62a70663
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2025-09-10 08:13:57 -0600

    update service region contract

--- src/main/java/com/customer/api/service/SvcRegion.java
+++ src/main/java/com/customer/api/service/SvcRegion.java
@@ -2,17 +2,18 @@ package com.customer.api.service;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
-
 import com.customer.api.dto.DtoRegionIn;
 import com.customer.api.entity.Region;
 import com.customer.commons.dto.ApiResponse;
 
 public interface SvcRegion {
 
-	public ResponseEntity<List<Region>> getRegions();
-	public ResponseEntity<ApiResponse> createRegion(DtoRegionIn in);
-	public ResponseEntity<ApiResponse> updateRegion(DtoRegionIn in, Integer id);
-	public ResponseEntity<ApiResponse> enableRegion(Integer id);
-	public ResponseEntity<ApiResponse> disableRegion(Integer id);
+	public List<Region> findAll();
+	public List<Region> findActive();
+	public ApiResponse create(DtoRegionIn in);
+	public ApiResponse update(DtoRegionIn in, Integer id);
+	public ApiResponse enable(Integer id);
+	public ApiResponse disable(Integer id);
 }
+
+
