49452501bc611d319e054afdbd27f7c05609e79d
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2025-10-19 14:40:33 -0600

    feature/swagger

--- src/main/java/com/customer/api/controller/CtrlRegion.java
+++ src/main/java/com/customer/api/controller/CtrlRegion.java
@@ -18,41 +18,50 @@ import com.customer.api.entity.Region;
 import com.customer.api.service.SvcRegion;
 import com.customer.commons.dto.ApiResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/region")
+@Tag(name = "Region", description = "Catálogo de regiones")
 public class CtrlRegion {
 	
 	@Autowired
 	SvcRegion svc;
 
 	@GetMapping
+	@Operation(summary = "Consultar regiones", description = "Consulta todas las regiones registradas")
 	public ResponseEntity<List<Region>> findAll(){
 		return ResponseEntity.ok(svc.findAll());
 	}
 	
 	@GetMapping("/active")
+	@Operation(summary = "Consultar regiones activas", description = "Consulta las regiones activas registradas")
 	public ResponseEntity<List<Region>> findActive(){
 		return ResponseEntity.ok(svc.findActive());
 	}
 	
 	@PostMapping
+	@Operation(summary = "Registrar regiones", description = "Registra una nueva región")
 	public ResponseEntity<ApiResponse> create(@Valid @RequestBody DtoRegionIn in) {
 		return ResponseEntity.ok(svc.create(in));
 	}
 	
 	@PutMapping("/{id}")
+	@Operation(summary = "Actualizar regiones", description = "Actualiza una región")
 	public ResponseEntity<ApiResponse> update(@Valid @RequestBody DtoRegionIn in, @PathVariable("id") Integer id){
 		return ResponseEntity.ok(svc.update(in, id));
 	}
 
 	@PatchMapping("/{id}/enable")
+	@Operation(summary = "Activar regiones", description = "Activa una región desactivada")
 	public ResponseEntity<ApiResponse> enable(@PathVariable Integer id) {
 		return ResponseEntity.ok(svc.enable(id));
 	}
 
 	@PatchMapping("/{id}/disable")
+	@Operation(summary = "Desactivar regiones", description = "Desactiva una región registrada")
 	public ResponseEntity<ApiResponse> disable(@PathVariable Integer id) {
 		return ResponseEntity.ok(svc.disable(id));
 	}
